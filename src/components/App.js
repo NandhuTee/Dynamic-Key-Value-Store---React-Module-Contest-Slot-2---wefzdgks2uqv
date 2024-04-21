8a9cda2bad34e2f90f90793f9165e095b431c05d
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-21 17:47:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import KeyValueStore from './KeyValueStore';
 import '../styles/App.css';
 
 function App() {
-  return (
-    <Router>
-      <div id="main">
-        <nav>
-          Reset
-        </nav>
-        <KeyValueStore />
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div id="main">
+                <nav>
+                    <Link to="/">Reset</Link>
+                </nav>
+                <Route path="/" exact>
+                    <KeyValueStore />
+                </Route>
+            </div>
+        </Router>
+    );
 }
-
 
 export default App;
